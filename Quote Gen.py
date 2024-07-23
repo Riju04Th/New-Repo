33f08608040c54596d0e87d9a7945246eabb36d0
Subhasish Addya <riju041199@gmail.com>
2024-07-23 19:13:49 +0530

    first commit

--- Quote Gen.py
+++ Quote Gen.py
@@ -10,7 +10,17 @@ def generate_quote():
         "The mind is everything. What you think you become.",
         "Your time is limited, don’t waste it living someone else’s life.",
         "You only live once, but if you do it right, once is enough.",
-        "The journey of a thousand miles begins with one step."
+        "The journey of a thousand miles begins with one step.",
+        "In our society, those who don't have many abilities tend to complain more.",
+        "It's not the face that makes someone a monster; it's the choices they make with their lives.",
+        "A lesson without pain is meaningless. For you cannot gain something without sacrificing something else in return.",
+        "People’s lives don’t end when they die. It ends when they lose faith.",
+        "If you don’t take risks, you can’t create a future.",
+        "Whatever you lose, you’ll find it again. But what you throw away you’ll never get back.",
+        "Fear is not evil. It tells you what your weakness is. And once you know your weakness, you can become stronger as well as kinder.",
+        "The world isn’t perfect. But it’s there for us, doing the best it can….that’s what makes it so damn beautiful.",
+        "A place where someone still thinks about you is a place you can call home.",
+        "No matter how deep the night, it always turns to day, eventually."
     ]
     return random.choice(quotes)
 if __name__ == "__main__":
