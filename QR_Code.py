b3651194075ab4d4645c3d9f84d395c2685a7a4e
Subhasish Addya <riju041199@gmail.com>
2024-07-25 15:03:48 +0530

    first commit

--- QR_Code.py
+++ QR_Code.py
@@ -3,5 +3,5 @@ from PIL import Image
 qr=qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
 qr.add_data("")
 qr.make(fit=True)
-img=qr.make_image(fill_color="red", back_color="cyan", border="tight")
+img=qr.make_image(fill_color="red", back_color="cyan")
 img.save("NewImage.png")
