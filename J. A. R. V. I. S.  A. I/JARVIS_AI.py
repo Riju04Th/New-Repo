a76ad5f1100efa6be9ca07b03b719fe1dbc2b757
Subhasish Addya <riju041199@gmail.com>
2024-08-09 17:01:33 +0530

    first commit

--- J. A. R. V. I. S.  A. I/JARVIS_AI.py
+++ J. A. R. V. I. S.  A. I/JARVIS_AI.py
@@ -1,9 +1,9 @@
 import pyttsx3
-# import speech_recognition as sr
+import speech_recognition as sr
 import datetime
-import os
-import wikipedia
-import pyaudio
+# import os
+# import wikipedia
+# import pyaudio
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[0].id)
@@ -26,22 +26,22 @@ def Greetings():
         speak("Good Night!")
     print("I am EDITH Sir. Please tell me how may I help you")
     speak("I am EDITH Sir. Please tell me how may I help you")
-# def takeCommand():
-# #It takes microphone input from the user and returns string output
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print("Listening...")
-#         r.adjust_for_ambient_noise(source)
-#         r.pause_threshold = 0.5
-#         audio = r.listen(source)
-#     try:
-#         print("Recognizing...")    
-#         query = r.recognize_google(audio, language='en-in')
-#     except Exception as e:
-#         # print(e)    
-#         print("Say that again please...")  
-#         return None
-#     return query
+def takeCommand():
+#It takes microphone input from the user and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.adjust_for_ambient_noise(source)
+        r.pause_threshold = 0.5
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return None
+    return query
 if __name__ == "__main__":
     Greetings()
     query = input("enter command: ")
