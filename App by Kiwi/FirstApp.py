d6f7e3a53b0d4d8f7850cad452d0c00862e6e940
Subhasish Addya <riju041199@gmail.com>
2024-08-10 18:04:45 +0530

    first commit

--- App by Kiwi/FirstApp.py
+++ App by Kiwi/FirstApp.py
@@ -1,6 +1,20 @@
-# TODO: Create a function to get the current working directory
-import os
-def get_cwd():
-    # Use the os.getcwd() function to get the current working directory
-    cwd = os.getcwd()
-    return cwd
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+class MyApp(App):
+    def build(self):
+        self.title = "Kivy Simple App"
+        layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="Hello, Kivy!", font_size='40sp')
+        btn = Button(text="Click Me", font_size='30sp')
+        # Bind the button's on_press event to the update_label method
+        btn.bind(on_press=self.update_label)
+        layout.add_widget(self.label)
+        layout.add_widget(btn)
+        return layout
+    def update_label(self, instance):
+        self.label.text = "Button Clicked!"
+if __name__ == '__main__':
+    MyApp().run()
+# This is a simple Kivy app that creates a vertical layout with a label and a button. When the button is clicked, the label changes to "Button Clicked!".
