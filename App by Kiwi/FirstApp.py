8d3435d67fbc1a57836eaad6f4a09a76ee8a94cb
Subhasish Addya <riju041199@gmail.com>
2024-08-10 18:01:17 +0530

    first commit

--- App by Kiwi/FirstApp.py
+++ App by Kiwi/FirstApp.py
@@ -1,1 +1,6 @@
-print('hell-o')
+# TODO: Create a function to get the current working directory
+import os
+def get_cwd():
+    # Use the os.getcwd() function to get the current working directory
+    cwd = os.getcwd()
+    return cwd
