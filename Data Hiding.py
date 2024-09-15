36cf831c17604c57ebd049f2c86342ed024a88f4
Subhasish Addya <riju041199@gmail.com>
2024-09-15 11:05:40 +0630

    first

--- Data Hiding.py
+++ Data Hiding.py
@@ -6,21 +6,43 @@
 # count = Solution()
 # count.sum()
 # count.sum()
-# print(count.__privateCount)
+# print(count.__privateCounter)
 
 # can't use the private class function
 
-class Solution:
-	__privateCounter = 0
-	def sum(self):
-		self.__privateCounter += 1
-		print(self.__privateCounter)
-count = Solution()
-count.sum()
-count.sum()
-count.sum()
-count.sum()
-count.sum()
-print(count._Solution__privateCounter)
+# class Solution:
+# 	__privateCounter = 0
+# 	def sum(self):
+# 		self.__privateCounter += 1
+# 		print(self.__privateCounter)
+# count = Solution()
+# count.sum()
+# count.sum()
+# count.sum()
+# count.sum()
+# count.sum()
+# print(count._Solution__privateCounter)
 
 # use the private class function
+
+# class A:
+#     def __init__(self):
+#         self._p_var = 10
+#     def _p_m(self):
+#         print("protected variable")
+#     def pub_m(self):
+#         self._p_m()
+
+# obj = A()
+# obj.pub_m()
+
+class A:
+    def __init__(self):
+        self.__p_var = 10
+    def __p_m(self):
+        print("protected variable")
+    def pub_m(self):
+        self.__p_m()
+
+obj = A()
+obj.pub_m()
